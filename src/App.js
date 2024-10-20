117e8aa17d981bfbbd5590855e502389a20638ac
Skuise <73568776+Skuise@users.noreply.github.com>
2024-10-20 04:37:15 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import CommissionHomePage from './CommissionHomePage';
 import Gallery from './Gallery';
 import About from './About';
@@ -6,16 +7,18 @@ import Contact from './Contact';
 import Terms from './Terms';
 
 function App() {
-  const path = window.location.pathname;
-
   return (
-    <div className="App">
-      {path === '/' && <CommissionHomePage />}
-      {path === '/gallery' && <Gallery />}
-      {path === '/about' && <About />}
-      {path === '/contact' && <Contact />}
-      {path === '/terms' && <Terms />}
-    </div>
+    <HashRouter>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<CommissionHomePage />} />
+          <Route path="/gallery" element={<Gallery />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/terms" element={<Terms />} />
+        </Routes>
+      </div>
+    </HashRouter>
   );
 }
 
